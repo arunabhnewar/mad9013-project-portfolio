6628825aa830913e97c131a3253f6ea6278130e9
arunabhnewar <newardevil@gmail.com>
2023-12-12 12:20:40 +0600

    navbar css edit

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className='p-5 bg-white shadow md:flex md:items-center md:justify-between'>
+    <nav className='p-5 bg-[#161321] shadow md:flex md:items-center md:justify-between'>
       <div className='flex justify-between items-center'>
         <Link to='/'>
           <span className='text-2xl font-[Poppins] cursor-pointer flex items-center'>
@@ -20,44 +20,49 @@ const Navbar = () => {
               src='https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg'
               alt='Tailwind Logo'
             />
-            <p className='ml-2 text-3xl'>Portfolio</p>
+            <p className='ml-2 text-3xl text-[#DDDDCC]'>Portfolio</p>
           </span>
         </Link>
 
         <span
           className='text-3xl cursor-pointer mx-2 md:hidden block'
           onClick={toggleMenu}>
-          {isMenuOpen ? <IoCloseSharp /> : <IoMdMenu />}
+          {isMenuOpen ? (
+            <IoCloseSharp className='text-[#DDDDCC]' />
+          ) : (
+            <IoMdMenu className='text-[#DDDDCC]' />
+          )}
         </span>
       </div>
 
       <ul
-        className={`md:flex md:items-center z-[1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 ${
-          isMenuOpen ? "opacity-100 top-100" : "opacity-0 top-[-400px]"
-        } transition-all ease-in duration-500 bg-red-600`}>
+        className={`md:flex md:items-center z-[1] md:z-auto md:static absolute  w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 ${
+          isMenuOpen
+            ? "opacity-100 top-100 bg-[#161321] "
+            : "opacity-0 top-[-400px] bg-[#161321] "
+        } transition-all ease-in duration-500 bg-[#1D1A29] `}>
         <li className='mx-4 my-6 md:my-0'>
-          <Link to='/' className='text-xl hover:text-cyan-500 duration-500'>
+          <Link
+            to='/'
+            className='text-xl hover:text-cyan-500 duration-500 text-[#DDDDCC]'>
             HOME
           </Link>
         </li>
         <li className='mx-4 my-6 md:my-0'>
           <Link
             to='portfolio'
-            className='text-xl hover:text-cyan-500 duration-500'>
+            className='text-xl hover:text-cyan-500 duration-500 text-[#DDDDCC]'>
             PORTFOLIO
           </Link>
         </li>
         <li className='mx-4 my-6 md:my-0'>
           <Link
             to='contact'
-            className='text-xl hover:text-cyan-500 duration-500'>
+            className='text-xl hover:text-cyan-500 duration-500 text-[#DDDDCC]'>
             CONTACT
           </Link>
         </li>
 
-        {/* <button className='bg-cyan-400 text-white font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded'>
-          Get started
-        </button> */}
         <h2 className=''></h2>
       </ul>
     </nav>
