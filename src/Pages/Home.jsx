ed07ce48573e5e5f1f5450252198275955508be8
JoySarkarBD <developer.joysarkar@gmail.com>
2023-12-12 14:09:07 +0600

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
-import BannerTwo from "../Components/Banner/Banner";
+import Banner from "../Components/Banner/Banner";
+// import BannerTwo from "../Components/BannerTwo/BannerTwo";
 import TechnologiesCard from "../Components/TechnologiesCard/TechnologiesCard";
 
 const Home = () => {
   return (
     <>
-      <BannerTwo />
+      <Banner />
+      {/* <BannerTwo /> */}
       <TechnologiesCard />
     </>
   );
